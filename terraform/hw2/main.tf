ff6c7a108b75be51e26ec9b2e441203fb8903fee
Andrii Bondar <andrey.bondar.ua@gmail.com>
2020-10-25 17:45:47 +0200

    add k8s provision for lab2

---
+++ terraform/hw2/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = var.region
+
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["*ubuntu-focal-20.04-amd64-server-*"] # or "*ubuntu-bionic-18.04-amd64-server-*"
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+module "ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 2.0"
+
+  name           = "hw2_stack"
+  instance_count = var.instance_count
+  # ami                  = "ami-04932daa2567651e7" # Ubuntu
+  ami                    = data.aws_ami.latest-ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  subnet_id              = tolist(data.aws_subnet_ids.all.ids)[0]
+  vpc_security_group_ids = ["sg-021bfd8e5a5d21433"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "hw2"
+  }
+}
+
+
