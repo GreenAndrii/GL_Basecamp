25f54c413c51178b8be7630b365f3e7cda1f0454
Andrii Bondar <andrey.bondar.ua@gmail.com>
2020-11-26 16:34:24 +0200

    fix problem number starts with zero

--- bash/Calc/my_math.sh
+++ bash/Calc/my_math.sh
@@ -1,34 +1,33 @@
 # math function for Calc
 function MY_MATH_FLOAT(){
-local A operation B
-A=$(echo $1 | tr -d '0+$' | tr -d '^0+')
-operation=$2
-B=$(echo $3 | tr -d '0+$' | tr -d '^0+')
+local A=$1
+local operation=$2
+local B=$3
 if [[ $(expr index $A '.') -eq 0 && $(expr index $B '.') -eq 0 &&  "$operation" != "/" ]];
   then
     echo $(expr $A "$operation" $B)
   else
-    [[ $(expr index $A '.') -ne 0 ]] && dot_A=$((${#A} - $(expr index $A '.'))) || dot_A=0
-    [[ $(expr index $B '.') -ne 0 ]] && dot_B=$((${#B} - $(expr index $B '.'))) || dot_B=0
+    [[ $(expr index $A '.') -ne 0 ]] && A=$(echo $A | sed '/\./ s/\.\{0,1\}0\{1,\}$//') && dot_A=$((${#A} - $(expr index $A '.'))) || dot_A=0
+    [[ $(expr index $B '.') -ne 0 ]] && B=$(echo $B | sed '/\./ s/\.\{0,1\}0\{1,\}$//') && dot_B=$((${#B} - $(expr index $B '.'))) || dot_B=0
     add_A=$(($DIGITS - $dot_A))
     add_B=$(($DIGITS - $dot_B))
     [[ $dot_A -ge $dot_B ]] && dot_comm=$dot_A || dot_comm=$dot_B
-    A=$(echo $A | tr -d '.')
-    B=$(echo $B | tr -d '.'| tr -d '^0+')
+    A=$(echo $A | sed 's/^0\{1,\}\.\{0,1\}//' | tr -d '.') #cut zeros at the begining and dot
+    B=$(echo $B | sed 's/^0\{1,\}\.\{0,1\}//' | tr -d '.')
     case "$operation" in
       '+'|'-')
-          printf %\."$dot_comm"f "$(expr $(($A * 10 ** $add_A)) "$operation" $(($B * 10 ** $add_B)))e-$DIGITS";;
+          printf %\."$dot_comm"f "$(expr $((10#$A * 10 ** $add_A)) "$operation" $((10#$B * 10 ** $add_B)))e-$DIGITS";;
       '*')
-          printf %\."$(($dot_A + $dot_B))"f "$(expr $(($A * 10 ** $dot_A)) "$operation" $(($B * 10 ** $dot_B)))e-$(($dot_A * 2 + $dot_B * 2))";;
+          printf %\."$(($dot_A + $dot_B))"f "$(expr $((10#$A * 10 ** $dot_A)) "$operation" $((10#$B * 10 ** $dot_B)))e-$(($dot_A * 2 + $dot_B * 2))";;
       '/')
-          printf %\."$DIGITS"f "$(expr $(($A * 10 ** $add_A)) "$operation" $B)e-$add_B";;
+          printf %\."$DIGITS"f "$(($((10#$A * 10 ** $add_A)) "$operation" 10#$B))e-$add_B";;
     esac
 fi
 }
 
 function help() {
   echo -e "Version: $VERSION\nEnter math expression for its calculation."
-  echo -e "use \"$0 Number1 +-/* Number2\""
+  echo -e "use \"$0 Number1+-/*Number2\""
 }
 
 rpn() {
